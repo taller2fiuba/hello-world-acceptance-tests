a702e0e36c64b5488baea490af106f3441b65ebd
Franco Liberali <franco.liberali@acadeu.com>
2020-04-25 18:51:21 -0300

    meter el test que aun no anda

--- features/steps/test-steps.py
+++ features/steps/test-steps.py
@@ -3,8 +3,8 @@ import requests
 
 @when('llamo al index')
 def step_impl(context):
-    context.response = requests.get('http://localhost:27080').json()
+    context.response = requests.get('http://localhost:5000').json()
 
 @then('devuelve hello world')
 def step_impl(context):
-    assert context.response == {'title': 'Hello world by NodeJS'}
+    assert context.response == {'title': '(Hello world by NodeJS) by Flask'}
